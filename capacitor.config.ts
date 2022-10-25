6fe6b4454fee32bb80def2f82f0c4255ec5867f4
martitasea <marta.pulido.asin@gmail.com>
2022-10-25 16:50:25 +0200

    Minor error

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,14 +4,10 @@ const config: CapacitorConfig = {
   appId: 'cat.icgc.catofflinev2',
   appName: ' Catalunya Offline v2',
   webDir: 'dist',
-<<<<<<< HEAD
-  bundledWebRuntime: false
-=======
   bundledWebRuntime: false,
   server: {
     errorPath: 'unsupported.html'
   }
->>>>>>> fcafd8ae509716ea5b6be863df00055e6f3050a0
 };
 
 export default config;
