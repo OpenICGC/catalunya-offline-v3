3945c9997fc20de55d57def763965881929004ee
martitasea <marta.pulido.asin@gmail.com>
2023-03-02 11:37:51 +0100

    Desplaza caja de búsqueda si se edita punto o traza

--- src/components/common/SearchBoxAndMenu.tsx
+++ src/components/common/SearchBoxAndMenu.tsx
@@ -11,6 +11,9 @@ import InfoIcon from '@mui/icons-material/Info';
 import {useTranslation} from 'react-i18next';
 import Paper from '@mui/material/Paper';
 import useTheme from '@mui/material/styles/useTheme';
+import {Theme} from '@mui/material';
+import useEditingPosition from '../../hooks/useEditingPosition';
+import useRecordingTrack from '../../hooks/useRecordingTrack';
 
 //TYPES
 export type SearchBoxAndMenuProps = {
@@ -31,14 +34,29 @@ const contextualMenu = [
   }
 ];
 
-const contextualMenuSx = {
+//STYLES
+const searchSx = (theme: Theme, isEditingPosition: boolean, isRecordingTrack: boolean) => ({
+  '&.SearchBox-root': {
+    zIndex: 1001,
+    position: 'absolute',
+    top: isEditingPosition || isRecordingTrack ? 48 : 0,
+    m: 1,
+    maxWidth: {
+      xs: `calc(100% - ${theme.spacing(2)})`,
+      md: '30vw'
+    },
+    right: 0
+  }
+});
+
+const contextualMenuSx = (isEditingPosition: boolean, isRecordingTrack: boolean) => ({
   mt: 1,
   boxShadow: 5,
   position: 'absolute',
-  top: 48,
+  top: isEditingPosition || isRecordingTrack ? 96 : 48,
   right: 8,
   zIndex: 1001
-};
+});
 
 const SearchBoxAndMenu: FC<SearchBoxAndMenuProps> = ({
   placeholder,
@@ -47,19 +65,8 @@ const SearchBoxAndMenu: FC<SearchBoxAndMenuProps> = ({
   const {t} = useTranslation();
   const theme = useTheme();
 
-  //STYLES
-  const searchSx = {
-    '&.SearchBox-root': {
-      zIndex: 1001,
-      position: 'absolute',
-      m: 1,
-      maxWidth: {
-        xs: `calc(100% - ${theme.spacing(2)})`,
-        md: '30vw'
-      },
-      right: 0
-    }
-  };
+  const isEditingPosition = !!useEditingPosition().position;
+  const isRecordingTrack = useRecordingTrack().isRecording;
 
   //CONTEXTUAL MENU
   const [isOpen, setIsOpen] = useState(false);
@@ -68,6 +75,7 @@ const SearchBoxAndMenu: FC<SearchBoxAndMenuProps> = ({
     setIsOpen(false);
     onContextualMenuClick(actionId);
   };
+
   return <>
     <SearchBox
       id='search-box'        
@@ -76,10 +84,10 @@ const SearchBoxAndMenu: FC<SearchBoxAndMenuProps> = ({
       onTextChange={() => console.log('writing')}
       onSearchClick={() => console.log('search')}
       placeholder={placeholder}
-      sx={searchSx}
+      sx={searchSx(theme, isEditingPosition, isRecordingTrack)}
     />
     {
-      isOpen && <Paper sx={contextualMenuSx}>
+      isOpen && <Paper sx={contextualMenuSx(isEditingPosition, isRecordingTrack)}>
         <MenuList dense sx={{p: 0}}>
           {
             contextualMenu?.map(({id, label, icon}) => <MenuItem key={id} onClick={() => handleAction(id)}>
