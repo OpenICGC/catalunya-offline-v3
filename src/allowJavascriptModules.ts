dfa3b80ce450813fe6f622cbe03aca5c6daaf300
Oscar Fonts <oscar.fonts@geomati.co>
2023-03-07 13:10:48 +0100

    aplica v3.0.2

--- src/allowJavascriptModules.ts
+++ src/allowJavascriptModules.ts
@@ -1,8 +1,5 @@
 // esto es un hack que viene de aqui https://blog.atomist.com/declaration-file-fix/
-//declare module '*';
-
-// Declare untyped modules, explicitly
-//declare module '@geomatico/geocomponents/*';
+//declare module 'nombre-de-la-libreria-que-no-tiene-tipado';
 
 declare module '*.xml' {
   const doc: string;
