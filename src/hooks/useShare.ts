50dc007047f949e93e4c147daa76fe49e59d53c0
Oscar Fonts <oscar.fonts@geomatico.es>
2023-05-03 11:45:00 +0200

    COF-244 | Compartir punt a instamaps (#131)

--- src/hooks/useShare.ts
+++ src/hooks/useShare.ts
@@ -2,12 +2,17 @@ import {useCallback} from 'react';
 import {ScopePoint} from '../types/commonTypes';
 import { Share } from '@capacitor/share';
 
-
 const useShare = () => {
 
   const sharePoint = useCallback(async (point: ScopePoint) => {
+    const fons = 'naturalMap';
+    const lat = point.geometry.coordinates[1].toFixed(5);
+    const lng = point.geometry.coordinates[0].toFixed(5);
+    const zoom = 16;
+    const text = encodeURIComponent(point.properties.name);
+
     await Share.share({
-      url: `https://www.openstreetmap.org/?mlat=${point.geometry.coordinates[1]}&mlon=${point.geometry.coordinates[0]}`
+      url: `https://www.instamaps.cat/visor.html?fons=${fons}&lat=${lat}&lng=${lng}&zoom=${zoom}&text=${text}`
     });
   }, []);
 
