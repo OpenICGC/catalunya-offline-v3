b80fe6a9919708a6f0d0730623bd023d1351526b
Oscar Fonts <oscar.fonts@geomati.co>
2023-05-25 12:42:05 +0200

    make component independent of custom hooks so it's purely presentational

--- src/components/common/GpsDisabledAlert.tsx
+++ src/components/common/GpsDisabledAlert.tsx
@@ -1,17 +1,15 @@
 import React, {FC, useCallback, useEffect, useState} from 'react';
-import usePointNavigation from '../../hooks/singleton/usePointNavigation';
-import useTrackNavigation from '../../hooks/singleton/useTrackNavigation';
-import useGeolocation from '../../hooks/singleton/useGeolocation';
-import useRecordingTrack from '../../hooks/singleton/useRecordingTrack';
+
+import TrackingIcon from '@mui/icons-material/MyLocation';
+import Typography from '@mui/material/Typography';
+import Dialog from '@mui/material/Dialog';
 import DialogTitle from '@mui/material/DialogTitle';
 import DialogContent from '@mui/material/DialogContent';
-import Typography from '@mui/material/Typography';
 import DialogActions from '@mui/material/DialogActions';
-import AcceptButton from '../buttons/AcceptButton';
 
-import Dialog from '@mui/material/Dialog';
-import TrackingIcon from '@mui/icons-material/MyLocation';
 import {useTranslation} from 'react-i18next';
+
+import AcceptButton from '../buttons/AcceptButton';
 
 //STYLES
 const dialogSx = {
@@ -21,43 +19,62 @@ const dialogSx = {
   letterSpacing: 1.35
 };
 
-const GpsDisabledAlert: FC = () => {
+const actionsSx = {
+  position: 'absolute',
+  bottom: 0,
+  right: 0
+};
+
+const paperProps = {
+  sx: {
+    height: 'auto',
+    pb: 8
+  }
+};
+
+const trackingIconSx = {mr: 2};
+
+const dialogContentSx = {mt: 2};
+
+export type GpsDisabledAlertProps = {
+  isNavigatingToTrack: boolean,
+  isNavigatingToPoint: boolean,
+  isRecordingTrack: boolean,
+  isGeolocationAvailable: boolean
+}
+
+const GpsDisabledAlert: FC<GpsDisabledAlertProps> = ({isNavigatingToTrack, isNavigatingToPoint, isRecordingTrack, isGeolocationAvailable}) => {
   const {t} = useTranslation();
-  const pointNavigation = usePointNavigation();
-  const trackNavigation = useTrackNavigation();
-  const {geolocation} = useGeolocation();
-  const recordingTrack = useRecordingTrack();
 
   const [isDialogOpen, setDialogOpen] = useState<boolean>(false);
 
-  const showDialog = useCallback((condition: boolean) => setDialogOpen(condition && !geolocation.latitude && !geolocation.longitude), [geolocation.latitude, geolocation.longitude]);
-
-  const handleAccept = useCallback(() => setDialogOpen(false), []);
-
-  useEffect(() => {
-    showDialog(pointNavigation.target !== undefined);
-  }, [pointNavigation.target]);
+  const showDialog = useCallback(() => setDialogOpen(true), []);
+  const hideDialog = useCallback(() => setDialogOpen(false), []);
 
   useEffect(() => {
-    showDialog(trackNavigation.target !== undefined);
-  }, [trackNavigation.target]);
+    isNavigatingToPoint && !isGeolocationAvailable && showDialog();
+  }, [isNavigatingToPoint]);
 
   useEffect(() => {
-    showDialog(recordingTrack.isRecording);
-  }, [recordingTrack.isRecording]);
+    isNavigatingToTrack && !isGeolocationAvailable && showDialog();
+  }, [isNavigatingToTrack]);
 
-  return isDialogOpen ? <Dialog open={isDialogOpen} onClose={handleAccept} fullWidth PaperProps={{sx: {height: 'auto', pb: 8}}}>
+  useEffect(() => {
+    isRecordingTrack && !isGeolocationAvailable && showDialog();
+  }, [isRecordingTrack]);
+
+  return isDialogOpen ? <Dialog open={isDialogOpen} onClose={hideDialog} fullWidth PaperProps={paperProps}>
     <DialogTitle sx={dialogSx}>
-      <TrackingIcon sx={{mr: 2}}/>
+      <TrackingIcon sx={trackingIconSx}/>
       {t('gpsAlert.title')}
     </DialogTitle>
-    <DialogContent sx={{mt: 2}}>
+    <DialogContent sx={dialogContentSx}>
       <Typography>
         {t('gpsAlert.message')}
       </Typography>
     </DialogContent>
-    <DialogActions sx={{position: 'absolute', bottom: 0, right: 0}}>
-      <AcceptButton onAccept={handleAccept}/>
+    <DialogActions sx={actionsSx}>
+      <AcceptButton onAccept={hideDialog}/>
     </DialogActions>
   </Dialog> : null;
 };
